9a6e34ad61bea9fc93d3ab21500fbd44cfb1d016
Leandro Lourenço <105615180+leolourencof@users.noreply.github.com>
2024-01-16 01:09:14 -0300

    update main.ts

--- app/src/main.ts
+++ app/src/main.ts
@@ -7,11 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
   app.use(cookieParser());
-  app.enableCors({
-    credentials: true,
-    origin: ['http://localhost:3001', 'https://keyslab.vercel.app/'],
-  });
 
   await app.listen(3000);
 }
