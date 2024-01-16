aea4eef3da1538db041b2ca9be57fa346dbf3d42
Leandro Lourenço <105615180+leolourencof@users.noreply.github.com>
2024-01-16 00:56:29 -0300

    update: updated cors

--- app/src/main.ts
+++ app/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors();
+  app.enableCors({ credentials: true, origin: '*' });
   app.use(cookieParser());
 
   await app.listen(3000);
